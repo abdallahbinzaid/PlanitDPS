1e58d199768ed73906571dd4279713d87dba58c0
Cesar Perdomo <cesarp1080@gmail.com>
2025-03-16 15:16:54 -0600

    importacion de bootstrap, estilos globales

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,22 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import BootstrapClient from './bootstrapClient';
+"use client"; //aca indicamos que es un componente client, porque se usara internamente
 
-export const metadata = {
-  title: 'Proyectos DPS',
-  description: 'Proyecto Next.js con App Router',
-};
+import 'bootstrap/dist/css/bootstrap.min.css'; // importamos bootstrap
+import "./styles/globals.css"; //importamos estilos globales
+import Navbar from "@/components/Navbar"; //componente de navegacion se creara en otro archivo
+import Footer from "@/components/Footer"; //componente de footer se creara en otro archivo
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="es">
+      <head>
+        <title>Sistema de Gestion de Proyectos con React/Next</title>
+      </head>
       <body>
-        {children}
+        <Navbar />
+        <main className="container my-4">
+          {children}
+        </main>
+        <Footer />
       </body>
     </html>
   );
