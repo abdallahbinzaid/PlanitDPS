fb4e379a05e412d160bae4f997012db9677ec2fd
Cesar Perdomo <cesarp1080@gmail.com>
2025-03-16 15:29:39 -0600

    se agrega pagina de inicio

---
+++ app/dashboard/page.js
@@ -0,0 +1,11 @@
+// app/dashboard/page.js
+"use client";
+
+export default function DashboardPage() {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <p>Bienvenido al Sistema de Gestión de Proyectos DPS</p>
+    </div>
+  );
+}
