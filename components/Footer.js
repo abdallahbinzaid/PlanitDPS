b827c13d3acc12d312dfeb31c2ad661f72eee762
Cesar Perdomo <cesarp1080@gmail.com>
2025-03-16 19:14:31 -0600

    se agrega footer

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Footer() {
+  return (
+    <footer>
+      <p>&copy; 2025 Gestionadora de Proyectos todos los derechos reservados</p>
+    </footer>
+  );
+}
