d46ee0c320a9e78cb99a6ce7020e5a02db86c087
Cesar Perdomo <cesarp1080@gmail.com>
2025-03-16 19:11:32 -0600

    se corrige errores en dashboard

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,11 +1,29 @@
 // app/dashboard/page.js
 "use client";
 
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { useAuth } from "@/context/AuthContext";
+
 export default function DashboardPage() {
+  const { user } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    // Si no hay usuario, redirige al login
+    if (!user) {
+      router.push("/auth/login");
+    }
+  }, [user, router]);
+
+  if (!user) {
+    return <p>Cargando...</p>;
+  }
+
   return (
     <div>
       <h1>Dashboard</h1>
-      <p>Bienvenido al Sistema de Gestión de Proyectos DPS</p>
+      <p>Bienvenido, {user.username}.</p>
     </div>
   );
 }
