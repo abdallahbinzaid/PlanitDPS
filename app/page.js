c8e545919a95cee41b2ad6fd1e816402c92342ce
Cesar Perdomo <cesarp1080@gmail.com>
2025-03-16 19:10:31 -0600

    se modifica pagina de inicio

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@
 export default function HomePage() {
     return (
       <main className="container py-4">
-        <h1 className="text-primary">¡Hola, Bootstrap!</h1>
+        <h1 className="text-primary">Agregar Proyecto/Tarea</h1>
         <button className="btn btn-success">Clic aquí</button>
       </main>
     );
